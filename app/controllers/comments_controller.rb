18e4ea1d7a0563f5a9d58603455f16e910751032
hongbin <fusis1@naver.com>
2021-05-14 10:26:23 -0700

    feat: add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,19 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :update, :destroy]
+  before_action :set_post, only: [:index, :create, :update, :destroy]
 
   # GET /comments
   def index
-    @comments = Comment.all
+    @comments = @post.comments
 
     render json: @comments
   end
 
-  # GET /comments/1
-  def show
-    render json: @comment
-  end
-
   # POST /comments
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @post.comments.create(comment_params)
 
     if @comment.save
-      render json: @comment, status: :created, location: @comment
+      render json: @comment, status: :created
     else
       render json: @comment.errors, status: :unprocessable_entity
     end
@@ -26,6 +21,7 @@ class CommentsController < ApplicationController
 
   # PATCH/PUT /comments/1
   def update
+    @comment = @post.comments.find(params[:id])
     if @comment.update(comment_params)
       render json: @comment
     else
@@ -35,17 +31,18 @@ class CommentsController < ApplicationController
 
   # DELETE /comments/1
   def destroy
+    @comment = @post.comments.find(params[:id])
     @comment.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
+    def set_post
+      @post = Post.find(params[:post_id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def comment_params
-      params.require(:comment).permit(:content, :post_id)
+      params.require(:comment).permit(:content)
     end
 end
